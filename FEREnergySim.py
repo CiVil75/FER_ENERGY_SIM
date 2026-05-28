5aa110b8188d607b2a9444871daacff25e83774d
CiVil75 <78097795+CiVil75@users.noreply.github.com>
2026-05-28 18:23:53 +0200

    Add files via upload

--- FEREnergySim.py
+++ FEREnergySim.py
@@ -325,8 +325,8 @@ if st.button(T["run_btn"], type="primary", use_container_width=True):
         prod = sim["fer"][i]
         
         diretto = min(prod, tot_load)
-        local_ac = directo
-        surplus, deficit = prod - directo, tot_load - directo
+        local_ac = diretto
+        surplus, deficit = prod - diretto, tot_load - diretto
         
         if surplus > 0 and battery_capacity_kwh > 0:
             ch = min(surplus * battery_eff, soc_max - soc_h_s1)
