a8420673aa507e38e635cb2d25013b611c580dc6
CiVil75 <78097795+CiVil75@users.noreply.github.com>
2026-06-08 19:04:25 +0200

    Update FEREnergySim.py

--- FEREnergySim.py
+++ FEREnergySim.py
@@ -1,5 +1,7 @@
 import streamlit as st
+
+# Configurazione iniziale obbligatoria
 st.set_page_config(page_title="RES-Based Home Simulator 8760", layout="wide")
 
-# Rimanda subito l'utente alla vera logica nascosta
+# Prova a passare solo il nome del file all'interno della cartella pages
 st.switch_page("pages/app_principale.py")
